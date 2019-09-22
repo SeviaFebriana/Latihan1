0d9bf836a54f20de166d0086acd8b0dd241bf1b4
SeviaFebriana <55661540+SeviaFebriana@users.noreply.github.com>
2019-09-22 23:12:01 +0700

    Add files via upload

---
+++ tugas.php.php
@@ -0,0 +1,59 @@
+<?php 
+$x="Nama:";
+$x.="Sevia ";
+$x.="Febriana";
+$y="NIM= ";
+$y.="11190930000034";
+echo $x;
+echo "<br>";
+echo $y;
+?>
+
+<?php
+echo "<br>";
+echo "<br>";
+echo "Segitiga"
+?>
+
+<?php
+$y="3";
+echo "<br>";
+echo "Alas=".$y;
+echo "<br>";
+echo "tinggi=".$y;
+echo "<br>";
+echo "luas=".$y*$y/2;
+?>
+
+<?php
+echo "<br>";
+echo "<br>";
+echo "Lingkaran";
+?>
+
+<?php
+$x="3.14";
+$y="7";
+echo "<br>";
+echo "Jari-Jari=".$y;
+echo "<br>";
+echo "phi=".$x;
+echo "<br>";
+echo "luas=".$x*$y*$y;
+?>
+
+<?php
+echo "<br>";
+echo "<br>";
+echo "Jajar Genjang";
+?>
+
+<?php
+$y="3";
+echo "<br>";
+echo "Alas=".$y;
+echo "<br>";
+echo "tinggi=".$y;
+echo "<br>";
+echo "luas=".$y*$y;
+?>
